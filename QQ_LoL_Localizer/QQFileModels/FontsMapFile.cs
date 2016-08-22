d511840f8a29ff322b0d84937a5cb827cee64cff
Thomas Barthelemy <Thomas.Barthelemy@outlook.com>
2016-08-23 06:34:59 +0700

    switched more font mapping to a chinese enabled font to avoid more case of font error

--- QQ_LoL_Localizer/QQFileModels/FontsMapFile.cs
+++ QQ_LoL_Localizer/QQFileModels/FontsMapFile.cs
@@ -10,13 +10,32 @@ namespace QQ_LoL_Localizer.QQFileModels
             get
             {
                 var content = File.ReadAllText(FilePath);
-                return !(content.Contains("arial.ttf")
-                         || content.Contains("verdana.ttf")
-                         || content.Contains("DejaVueSans.ttf")
-                         || content.Contains("FRQITC01.ttf")
-                         || content.Contains("Garuda.ttf")
-                         || content.Contains("UttumDotum.ttf")
-                         || content.Contains("SpiegelC"));
+                return !(
+                    // Bold fonts
+                    content.Contains("arialbd.ttf")
+                    || content.Contains("verdanab.ttf")
+                    || content.Contains("DejaVuSans-Bold.ttf")
+                    || content.Contains("FRQITCBT.ttf")
+                    || content.Contains("Garuda-Bold.ttf")
+                    || content.Contains("SpiegelCDB.otf")
+                    || content.Contains("Spiegel-Bold.otf")
+                    || content.Contains("BeaufortforLOL-Bold.otf")
+
+                    || content.Contains("arial.ttf")
+                    || content.Contains("arialbk.ttf")
+                    || content.Contains("verdana.ttf")
+                    || content.Contains("times.ttf")
+                    || content.Contains("tahoma.ttf")
+                    || content.Contains("DejaVueSans.ttf")
+                    || content.Contains("FRQITC01.ttf")
+                    || content.Contains("Garuda.ttf")
+                    || content.Contains("UTMEssendineCaps.ttf")
+                    || content.Contains("UttumDotum.ttf")
+                    || content.Contains("GillSansMTPro-Medium.otf")
+                    || content.Contains("Spiegel-Regular.otf")
+                    || content.Contains("Spiegel-SemiBold.otf")
+                    || content.Contains("AxisStd-Regular.otf")
+                );
             }
             set
             {
@@ -39,12 +58,13 @@ namespace QQ_LoL_Localizer.QQFileModels
                 Helper.ReplaceInFile(FilePath, "FRQITCBT.ttf", "FZDHTK.ttf");
                 Helper.ReplaceInFile(FilePath, "Garuda-Bold.ttf", "FZDHTK.ttf");
                 Helper.ReplaceInFile(FilePath, "SpiegelCDB.otf", "FZDHTK.ttf");
-                Helper.ReplaceInFile(FilePath, "LucasFonts-SpiegelCdOT-SemiBold.otf", "FZDHTK.ttf");
+                Helper.ReplaceInFile(FilePath, "Spiegel-Bold.otf", "FZDHTK.ttf");
+                Helper.ReplaceInFile(FilePath, "BeaufortforLOL-Bold.otf", "FZDHTK.ttf");
 
                 // Regular fonts
                 Helper.ReplaceInFile(FilePath, "arial.ttf", "FZDHTK.ttf");
                 Helper.ReplaceInFile(FilePath, "ariblk.ttf", "FZDHTK.ttf");
-                Helper.ReplaceInFile(FilePath, "verdana.ttf", "FZDHTK.ttf");
+                Helper.ReplaceInFile(FilePath, "DATA\\\\Fonts\\\\verdana.ttf", "FZDHTK.ttf");
                 Helper.ReplaceInFile(FilePath, "times.ttf", "FZDHTK.ttf");
                 Helper.ReplaceInFile(FilePath, "tahoma.ttf", "FZDHTK.ttf");
                 Helper.ReplaceInFile(FilePath, "DejaVuSans.ttf", "FZDHTK.ttf");
@@ -52,7 +72,12 @@ namespace QQ_LoL_Localizer.QQFileModels
                 Helper.ReplaceInFile(FilePath, "Garuda.ttf", "FZDHTK.ttf");
                 Helper.ReplaceInFile(FilePath, "UTMEssendineCaps.ttf", "FZDHTK.ttf");
                 Helper.ReplaceInFile(FilePath, "UttumDotum.ttf", "FZDHTK.ttf");
-                Helper.ReplaceInFile(FilePath, "LucasFonts-SpiegelCdOT-Regular.otf", "FZDHTK.ttf");
+                Helper.ReplaceInFile(FilePath, "GillSansMTPro-Medium.otf", "FZDHTK.ttf");
+                Helper.ReplaceInFile(FilePath, "Spiegel-Regular.otf", "FZDHTK.ttf");
+                Helper.ReplaceInFile(FilePath, "Spiegel-SemiBold.otf", "FZDHTK.ttf");
+                Helper.ReplaceInFile(FilePath, "BeaufortforLOL-Regular.otf", "FZDHTK.ttf");
+                Helper.ReplaceInFile(FilePath, "AxisStd-Regular.otf", "FZDHTK.ttf");
+
                 IsFixed = null;
             });
         }
